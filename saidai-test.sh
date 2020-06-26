5bfdbc157e3b7612a81687765c1fa6d7c352f081
name <a2001yi@aiit.ac.jp>
2020-06-26 22:16:56 +0900

    first commit kadai4 shellscript

---
+++ saidai-test.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+ans="/tmp/$$-ans"
+result="/tmp/$$-result"
+err="/tmp/$$-error"
+
+#テスト内容（2つの引数はそれぞれa,bと表記）
+#正常動作
+#a>bの時
+./saidai.sh 18 27 > ${result}
+echo "最大公約数は 9" > ${ans}
+diff ${result} ${ans} || echo "1-1 a>b 失敗" >> ${err}
+
+#a<bの時
+./saidai.sh 15 10 > ${result}
+echo "最大公約数は 5" > ${ans}  
+diff ${result} ${ans} || echo "1-2 a<b 失敗" >> ${err} 
+
+#素数同士の時
+./saidai.sh 3 5 > ${result}
+echo "最大公約数は 1" > ${ans}  
+diff ${result} ${ans} || echo "1-3 素数 失敗" >> ${err}
+
+#異常動作
+#0が含まれる
+./saidai.sh 0 20 > ${result}
+echo "error" > ${ans}
+diff ${result} ${ans} || echo "2-1 a=0 失敗" >> ${err} 
+
+./saidai.sh 114 0 > ${result} 
+echo "error" > ${ans} 
+diff ${result} ${ans} || echo "2-2 b=0 失敗" >> ${err}
+     
+./saidai.sh 0 0 > ${result}
+echo "error" > ${ans}
+diff ${result} ${ans} || echo "2-3 a=b=0 失敗" >> ${err}
+
+#負の数が含まれる
+./saidai.sh -1 5 > ${result}
+echo "error" > ${ans}     
+diff ${result} ${ans} || echo "2-4 a<0 失敗" >> ${err} 
+
+./saidai.sh -1 -98 > ${result} 
+echo "error" > ${ans} 
+diff ${result} ${ans} || echo "2-5 a<0 b<0 失敗" >> ${err} 
+
+./saidai.sh -999 -0 > ${result} 
+echo "error" > ${ans} 
+diff ${result} ${ans} || echo "2-6 a<0 b=0 失敗" >> ${err} 
+
+#引数が足りない
+./saidai.sh 46 > ${result} 
+echo "引数は2つ指定してください" > ${ans}
+diff ${result} ${ans} || echo "2-7 引数1つの場合 失敗" >> ${err} 
+
+./saidai.sh  > ${result} 
+echo "引数は2つ指定してください" > ${ans}
+diff ${result} ${ans} || echo "2-8 引数0の場合 失敗" >> ${err}
+
+if [ -f ${err} ]; then
+	cat ${err}
+	rm /tmp/$$-*
+	exit 1
+fi
+
---
+++ saidai.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#引数個数チェック
+if [ ! $# -eq 2 ]; then
+	echo "引数は2つ指定してください"
+	exit 1
+fi
+#最大公約数の計算
+a=$1
+b=$2
+if [ $a -le 0 -o $b -le 0 ]; then
+	echo "error"
+	exit 1
+fi
+r=`expr $a % $b`
+if [ $r = 0 ]; then
+	echo "最大公約数は $b"
+	exit 0
+else
+	while [ ! $r = 0 ];
+	do
+		a=$b
+		b=$r
+		r=`expr $a % $b`
+	done
+	echo "最大公約数は $b"
+	exit 0
+fi
+
